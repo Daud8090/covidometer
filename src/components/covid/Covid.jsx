4b6db761ed6881a37cdae99a20c2fa6d9e81ea6b
Daud <mohdrehman8090@gmail.com>
2021-12-21 20:50:56 +0530

    6th commit

--- src/components/covid/Covid.jsx
+++ src/components/covid/Covid.jsx
@@ -5,16 +5,16 @@ function Covid() {
         <div className='fixed-bg'>
             <div class="yourhealth">
                 <h1> <strong className='heading'>COVID-19 Testing: What You Need to Know </strong> </h1>
-                <h5> Testing is very important to help reduce the spread of COVID-19 </h5>
+                <h5 className="paragraph"> Testing is very important to help reduce the spread of COVID-19 </h5>
                 <h2> Types of COVID-19 Tests </h2>
-                <p class="paragraph">COVID-19 tests can detect either SARS-CoV-2, the virus that causes COVID-19, or antibodies that your body makes after getting COVID-19 or after getting vaccinated.
+                <p className="paragraph">COVID-19 tests can detect either SARS-CoV-2, the virus that causes COVID-19, or antibodies that your body makes after getting COVID-19 or after getting vaccinated.
                     Tests for SARS-CoV-2 tell you if you have an infection at the time of the test. This type of test is called a “viral” test because it looks for viral infection. Antigen or Nucleic Acid Amplification Tests (NAATs) are viral tests.
                     Tests for antibodies may tell you if you have had a past infection with the virus that causes COVID-19. Your body creates antibodies after getting infected with SARS-CoV-2 or after getting vaccinated against COVID-19. These tests are called “antibody” or “serology” tests.</p>
                 <h2>Viral Tests</h2>
-                <p>A viral test tells you if you are infected with SARS-CoV-2, the virus that causes COVID-19. There are two types of viral tests: rapid tests and laboratory tests. Viral tests use samples that come from your nose or mouth. Rapid tests can be performed in minutes and can include antigen and some NAATs. Laboratory tests can take days to complete and include RT-PCR and other types of NAATs. Some test results may need confirmatory testing.
+                <p className="paragraph">A viral test tells you if you are infected with SARS-CoV-2, the virus that causes COVID-19. There are two types of viral tests: rapid tests and laboratory tests. Viral tests use samples that come from your nose or mouth. Rapid tests can be performed in minutes and can include antigen and some NAATs. Laboratory tests can take days to complete and include RT-PCR and other types of NAATs. Some test results may need confirmatory testing.
                     Self-tests are rapid tests that can be taken at home or anywhere, are easy to use, and produce rapid results. COVID-19 self-tests are one of many risk-reduction measures, along with vaccination, masking, and physical distancing, that protect you and others by reducing the chances of spreading COVID-19.</p>
                 <h2> Antibody Tests </h2>
-                <p> An antibody test (also known as a serology test) can detect antibodies to SARS-CoV-2 in your blood. Antibodies are proteins that your immune system makes to help fight infection and protect you from getting sick in the future.
+                <p className="paragraph"> An antibody test (also known as a serology test) can detect antibodies to SARS-CoV-2 in your blood. Antibodies are proteins that your immune system makes to help fight infection and protect you from getting sick in the future.
                     Antibody tests should not be used to diagnose a current infection, but may indicate if you had a past infection. Antibody tests help scientists learn about how human immune systems defend against the virus, as well as learn about population-level protection.
                     Antibody testing is not currently recommended to determine:
                     <ul>
@@ -25,7 +25,7 @@ function Covid() {
                         <li>You should always discuss your test results with your healthcare provider.</li></ul>
                 </p>
                 <h2>Video Resources</h2>
-                <p> go through this link for more understanding <a href="https://youtu.be/v8ITXt1Cfas">vedio link 1</a>
+                <p className="paragraph"> go through this link for more understanding <a href="https://youtu.be/v8ITXt1Cfas">vedio link 1</a>
                     <a href="https://youtu.be/aHXvLRKViAk"> vedio link 2</a>
                 </p>
             </div>
